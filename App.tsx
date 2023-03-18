f36e1629d30f6dc849c2d513c526e93b310a661d
Italo Araujo <italoasouzat.i@gmail.com>
2023-03-18 12:47:50 -0300

    animando saida de item com swipeable

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
-export default function App() {
+export default function App () {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   if (!fontsLoaded) {
@@ -14,13 +16,15 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView
+      style={{ flex: 1 }}
+    >
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
-import { HouseLine } from 'phosphor-react-native';
+import { View, ScrollView, Alert, Pressable } from 'react-native';
+import { HouseLine, Trash } from 'phosphor-react-native';
 import Animated, { Layout, SlideInRight, SlideOutRight } from 'react-native-reanimated';
+import { Swipeable } from 'react-native-gesture-handler';
 
 import { Header } from '../../components/Header';
 import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
@@ -10,12 +11,14 @@ import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
 import { styles } from './styles';
 import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
 import { Loading } from '../../components/Loading';
+import { THEME } from '../../styles/theme';
 
 export function History () {
   const [isLoading, setIsLoading] = useState(true);
   const [history, setHistory] = useState<HistoryProps[]>([]);
 
   const { goBack } = useNavigation();
+  const swipeableRefs = useRef<Swipeable[]>([]);
 
   async function fetchHistory () {
     const response = await historyGetAll();
@@ -25,11 +28,11 @@ export function History () {
 
   async function remove (id: string) {
     await historyRemove(id);
-
     fetchHistory();
   }
 
-  function handleRemove (id: string) {
+  function handleRemove (id: string, index: number) {
+    swipeableRefs.current?.[index].close();
     Alert.alert(
       'Remover',
       'Deseja remover esse registro?',
@@ -65,24 +68,42 @@ export function History () {
         showsVerticalScrollIndicator={false}
       >
         {
-          history.map((item) => (
+          history.map((item, index) => (
             <Animated.View
               key={item.id}
               entering={SlideInRight}
               exiting={SlideOutRight}
               layout={Layout.springify()}
             >
-
-              <TouchableOpacity
-
-                onPress={() => handleRemove(item.id)}
+              <Swipeable
+                ref={(ref) => {
+                  if (ref) {
+                    swipeableRefs.current.push(ref)
+                  }
+                }}
+                overshootLeft={false}
+                containerStyle={styles.swipeableContainer}
+                onSwipeableOpen={() => handleRemove(item.id, index)}
+                leftThreshold={5}
+                renderRightActions={() => null}
+                renderLeftActions={() => (
+                  <View
+                    style={styles.swipeableRemove}
+                  >
+                    <Trash
+                      size={32}
+                      color={THEME.COLORS.GREY_100}
+                    />
+                  </View>
+                )}
               >
+
                 <HistoryCard data={item} />
-              </TouchableOpacity>
+              </Swipeable>
             </Animated.View>
           ))
         }
       </ScrollView>
-    </View>
+    </View >
   );
 }
